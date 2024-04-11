99ca54ceb9f413f1dc8be64faf468c7a5ba6f734
Audrey Tabler <101310073+audreytabler@users.noreply.github.com>
2024-04-11 14:20:48 -0500

    update favico in webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -33,7 +33,7 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: 'src/assets', to: 'assets' },
       { from: 'pwa', to: '' },
-      { from: 'src/favicon.ico', to: '' }
+      //{ from: 'src/favicon.ico', to: '' }
     ]),
     new InjectManifest({
       swSrc: path.resolve(__dirname, '../pwa/sw.js')
